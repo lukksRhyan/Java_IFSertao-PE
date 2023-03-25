4ca507a69950f19660fac7bfbb4c8cf90d5dd5e5
Lucas Rhyan Alves da Silva <108754987+lukksRhyan@users.noreply.github.com>
2023-03-24 21:41:40 -0300

    Add files via upload

--- ListaGrafico/src/App.java
+++ ListaGrafico/src/App.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
+
 import javax.swing.*;
 public class App {
 
@@ -11,7 +13,7 @@ public class App {
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Faz com que o programa encerre quando fechar a janela
 				frame.setVisible(true);
 				frame.setIconImage(img.getImage());
-				frame.setExtendedState(Frame.MAXIMIZED_BOTH);//Tela cheia
+				//frame.setExtendedState(Frame.MAXIMIZED_BOTH);//Tela cheia
 				
 			}
 		});
@@ -22,16 +24,45 @@ public class App {
 class SimpleFrame extends JFrame{
 	public SimpleFrame(){
 		Toolkit kit = Toolkit.getDefaultToolkit();
+		
 		Dimension screenSize = kit.getScreenSize();
 		int screenWidth = (int)screenSize.getWidth();
 		int screenHeight =(int) screenSize.getHeight();
+		setSize(screenWidth/2, screenHeight/2);
+
 		Image icone = kit.getImage("assets/icon.gif");
 		this.setIconImage(icone);
+		
 		setTitle("Cunny");//UOGHHH 
-		setSize(screenWidth/2, screenHeight/2);
-		 
+		
+		SimpleComponent componente  = new SimpleComponent();
+		add(componente);
+		
+		Forma desenho = new Forma();
+		add(desenho);
 	}
 
 	//public static final int DEFAULT_WIDTH = 300;
 	//public static final int DEFAULT_HEIGHT = 200;
+}
+class SimpleComponent extends JComponent{
+	public void paintComponent(Graphics g){
+		g.drawString("Bites the Dust", 	MENSAGEM_X, MENSAGEM_Y);
+	}
+	public static final int MENSAGEM_X = 25;
+	public static final int MENSAGEM_Y = 25;
+}
+class Forma extends JComponent{
+	public void paintComponent(Graphics g){
+		Graphics2D g2 = (Graphics2D) g;
+		double limiteEsq = 100;
+		double limiteTopo = 100;
+		double largura = 200;
+		double altura = 200;
+		
+		Rectangle2D retangulo = new Rectangle2D.Double(limiteEsq, limiteTopo, largura, altura);
+		g2.draw(retangulo);
+
+
+	}
 }
