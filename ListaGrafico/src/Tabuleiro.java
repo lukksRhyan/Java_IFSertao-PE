607b1781f9f6e715a69507f94faec3c68e6dad6c
Lucas Rhyan Alves da Silva <108754987+lukksRhyan@users.noreply.github.com>
2023-05-03 21:22:03 -0300

    l

--- ListaGrafico/src/Tabuleiro.java
+++ ListaGrafico/src/Tabuleiro.java
@@ -1,46 +1,48 @@
 import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Tabuleiro(){
-    public Casa[][] casas;
+class Tabuleiro extends JFrame{
+    private JPanel painelTabuleiro;
+    private JPanel[][] casasTabuleiro = new JPanel[8][8];
 
-    Tabuleiro(){
-        this(8,800);
-    }
-    Tabuleiro(int tamanho, int dimensao){
-        setSize(dimensao,dimensao);
-        this.preencher(tamanho, dimensao);
-        setTitle("Tabuleiro Xadrez");
-        this.setVisible(true);
-    }
-    private void preencher(int tamanho, int dimensao){
-        boolean par = false;
-        for(int i = 0; i<8; i++){
-            for(int j = 0; j<8; j++){
-                Object cor = (par)? Color.WHITE : Color.BLACK;
-                this.casas[i][j] = new Casa(i+1,j+1, (dimensao/tamanho));
-                atual.setColor(cor)
-                atual = this.casas[i][j];
-                
+    public Tabuleiro(){
+        setTitle("Tabuleiro de Xadrez");
+        setSize(400,400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+        
+        painelTabuleiro = new JPanel(new GridLayout(8, 8));
+        contentPane.add(painelTabuleiro, BorderLayout.CENTER);
+        boolean cor = false;
+        for(int x = 0; x < 8; x++){
+            for(int y = 0; y < 8; y++){
+                casasTabuleiro[x][y] = new JPanel(new BorderLayout());
+                painelTabuleiro.add(casasTabuleiro[x][y]);
+                if (cor){
+                    casasTabuleiro[x][y].setBackground(Color.WHITE);
+                }else{
+                    casasTabuleiro[x][y].setBackground(Color.BLACK);
+                }
+                casasTabuleiro[x][y].add(new Peca());
+                cor = !cor;
             }
+            cor = !cor;
         }
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Tabuleiro();
     }
 }
-class Casa extends Frame{
-    private int latitude;
-    private int longitude;
-    private Object cor;
-
-    Casa(int longitude, int latitude, int dimensao){
-        this.longitude = longitude;
-        this.latitude = latitude;
-
-        setBounds();
+class Peca extends JButton{
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.setColor(Color.RED);
+        g.drawOval(5,5,35,35);
+        g.fillOval(5,5,35,35);
+        setOpaque(false);
+        setBackground(Color.RED);
     }
-    public int longitude (){
-        return this.longitude;
-    } 
-    public int latitude(){
-        return this.latitude;
-    }
-
 }
