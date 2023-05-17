209ec9871d5d86ec3d3e50f20f691d10a0bbbc2a
Lucas Rhyan Alves da Silva <108754987+lukksRhyan@users.noreply.github.com>
2023-05-17 15:07:52 -0300

    Add files via upload

--- ListaEncadeada/src/listaencadeada/ListaEncadeada.java
+++ ListaEncadeada/src/listaencadeada/ListaEncadeada.java
@@ -28,45 +28,52 @@ public class ListaEncadeada {
         this.ultimo = novo;//So aqui substitui o ultimo
         this.numeroDeMembros +=1;//Aumenta o tamanho da lista apos ter adicionado o membro
     }
-    public void Adicionar(Object elemento){
-        No novo = new No(elemento);
+    public void Adicionar(Object novoElemento){
+        No novo = new No(novoElemento);
         this.Adicionar(novo);
     }
-    public void AdicionarNoInicio(Object elemento){
-        No novo = new No(elemento);
+    public void AdicionarNoInicio(Object novoElemento){
+        No novo = new No(novoElemento);
         novo.setProximo(primeiro);
         primeiro = novo;
     }
-     public void inserir(No antes,Object elemento){
-        No novo = new No(elemento);
+     public void inserir(No alvo,Object novoElemento){
+        No novo = new No(novoElemento);
         No depois = alvo.getProximo();
 
-        antes.setProximo(novo);
-        novo.setAnterior(antes);
+        alvo.setProximo(novo);
+        novo.setAnterior(alvo);
 
         depois.setAnterior(novo);
         novo.setProximo(depois);
     }
-    public No busca(Object elemento){
+    public No busca(Object novoElemento){
         No atual = this.primeiro;
         while (atual.proximo!= null) {
-            if(atual.ocupante.equals(elemento)){
+            if(atual.elemento.equals(novoElemento)){
                 return atual;
             }
+            atual = atual.proximo;
         }
         return null;
     }
     public void substituir(Object antigo, Object novo){
         No alvo = this.busca(antigo);
-        alvo.ocupante = novo;
+        alvo.elemento = novo;
     }
     public int tamanho(){
-        return this.numeroDeMembros;
+        int total = 0;
+        No atual = this.primeiro;
+        while(atual.getProximo()!= null){
+            total++;
+            atual = atual.getProximo();
+        }
+        return total;
     }
     public No getPosicao(int pos){
         No atual = this.primeiro;
-        for(int i = 0; i <=pos;i++){
-            atual = atual.proximo;
+        for(int i = 1; (i <pos) && (atual.getProximo()!= null);i++){
+            atual = atual.getProximo();
         }
         return atual;
     }
@@ -79,7 +86,7 @@ public class ListaEncadeada {
         No atual = this.primeiro;
 		for(int nosLista = 0; atual.proximo != null; nosLista++) {
 			s.append(atual.toString());
-			s.append(", \n");
+			s.append(", ");
             atual = atual.proximo;
         }
 		if(this.numeroDeMembros > 0) {
@@ -88,57 +95,88 @@ public class ListaEncadeada {
 		s.append("]");
 		return s.toString();
     }
+    //Prova 17-05-2023
+    public void questao01(Object elemento){
 
+        int ocorrenciasElemento = 0;
+        int valoresMaiores = 0;
+        No atual = this.primeiro;
+        
+        for(int i =1; atual.getProximo()!=null;i++){
+            if(atual.getElemento() == elemento){
+                ocorrenciasElemento++;
+                System.out.println("Elemento encontrado na posicao:"+ i);
+            }else if((int) atual.getElemento() > (int) elemento){//converti para int porque o > nao existe para objeto generico
+                valoresMaiores++;
+            }
+            atual=atual.getProximo();
+        }
+        System.out.println("Numero de nos na lista: " + this.tamanho());
+        System.out.println("Numero de ocorrencias do elemento: "+ ocorrenciasElemento);
+        System.out.println("Casas com valores maiores que o elemento: "+ valoresMaiores);
+    }
+    static void questao02(ListaEncadeada L){
+        for(int i = L.tamanho(); i>=0;i--){
+            No atual = L.primeiro;
+            for(int j = 0; j<i; j++){
+                atual=atual.getProximo();
+            }
+            System.out.print(atual.getElemento()+ " , ");
+        }
+    }
+    static void questao03(ListaEncadeada L, ListaEncadeada M, int posicao){
+        No alvo = L.getPosicao(posicao);
+        No sequente = alvo.getProximo();
+        
+        alvo.setProximo(M.primeiro);
+        M.ultimo.setProximo(sequente);
+        sequente.setAnterior(M.ultimo);
+    }
+    static void questao04(ListaEncadeada L, Object antigo, Object novo){
+        No indo = L.primeiro;
+        No voltando = L.ultimo;
+        int i = 0;
+        while((indo.getProximo()!= null)){
+            if((int)indo.getElemento() == (int)antigo) indo.setElemento(novo);
+            indo = indo.getProximo();
+            if((int)voltando.getElemento() == (int)antigo) voltando.setElemento(0);
+            voltando= voltando.getAnterior();
+        }
+    }
     //Questao 01 lista 11/05/23
     public No getNoMeio(){//Percorrendo a lista e voltando na metade
         No atual = this.primeiro;
-        
+        int nosLista;
         //Indo
-        for(int nosLista = 0; atual.proximo != null; nosLista++){
-            nosLista += 1;
+        for(nosLista= 0;atual.proximo != null; nosLista++){
             atual = atual.proximo;
         }
         //Voltando
-        for(int m = nosLista; m> nosLista/2; m--){
-            atual = atual.anterior;
+        for(int m = nosLista; m> (nosLista/2); m--){
+            atual = atual.getAnterior();
         }
         return atual;
     }
-    public No noDoMeio(){//contando o numero de membros e percorrendo metade dele
-        No atual = this.primeiro;
-
-        
-        //Indo
-        for(int nosLista = 0; atual.proximo != null; nosLista++){
-            //nosLista += 1;
-            atual = atual.getProximo();
-        }
-
-        noMeio = this.primeiro;
-        for(int i= 0; i < nosLista/2; i++){
-            noMeio = noMeio.getProximo();    
-        }
-    }
 
     //Questao 02
-    public Lista ocorrencias(Object o){
-        Lista L = new Lista(2);
+    public void ocorrencias(Object o){
         No atual = this.primeiro;
         
         for(int nosLista = 0; atual.proximo != null; nosLista++){
-            if(atual.getElemento() == o){
-                L.adiciona(nosLista);
+            if(atual.elemento.equals(o)){
+                System.out.println("Objeto encontrado na posicao " + (nosLista+1));
             }
+            atual= atual.proximo;
         }
         
     }
     //Questao 03
     public void adicionarDepois(Object local, Object novo){
-        No alvo = busca(Object);
+        No alvo = busca(local);
         if(alvo != null){
             inserir(alvo, novo);
         }else{
-            System.out.Println();
+            System.out.println();
         }
 
     }
